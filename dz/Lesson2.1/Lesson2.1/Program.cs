51b465ffc03209f832c9d040bf324ab05775a829
timavik <103438439+timavik@users.noreply.github.com>
2024-12-02 09:27:34 +0300

    Add files via upload

---
+++ dz/Lesson2.1/Lesson2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double sum = a + b;
+            double difference = a - b;
+            double product = a * b;
+            double quotient = a / b;
+
+            Console.WriteLine();
+            Console.WriteLine("Результаты:");
+            Console.WriteLine("Сумма: {0}", sum);
+            Console.WriteLine("Разность: {0}", difference);
+            Console.WriteLine("Произведение: {0}", product);
+            Console.WriteLine("Частное: {0}", quotient);
+        }
+    }
+}
---
+++ dz/Lesson2/Lesson2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привет напиши соё имя");
+            Console.ReadLine();
+            Console.WriteLine("напиши свой возрост");
+            Console.ReadLine();
+            Console.WriteLine("напиши свой рост в метрах");
+            Console.ReadLine();
+        }
+    }
+}
